ced2445a284add01bcc8e2787e2eb953ea73017b
akanksha6082 <56785692+akanksha6082@users.noreply.github.com>
2020-10-26 11:54:32 +0530

    Update brand.php

--- brand.php
+++ brand.php
@@ -1,3 +1,32 @@
 <?php
-echo "brand";
+    require_once 'includes/header.php';
+    require_once "db_connect.php";
 ?>
+
+<?php 
+    $result = $connect->query("SELECT * FROM brands") or die($connect->error);
+?>
+<div class = "container">
+    <div>
+        <h1 style="position: relative;margin-top: 8%;">Manage Brands</h1>
+    </div>
+    <br><br>  
+    <div class = "row justify-content-center">
+        <table class= "table">
+            <thead>
+                <tr>
+                    <th>Brand ID</th>
+                    <th>Brand Name</th>
+                    <th colspan ="2">Action<th>
+                </tr>
+            </thead>
+            <?php while ($row = $result->fetch_assoc()): ?>
+            <tr>
+                <td><?php echo $row['brand_id'] ?></td>
+                <td><?php echo $row['brand_name'] ?></td>
+                <td><button></button> <button></button></td>
+            </tr>
+            <?php endwhile; ?>
+        </table>
+    </div> 
+</div>
